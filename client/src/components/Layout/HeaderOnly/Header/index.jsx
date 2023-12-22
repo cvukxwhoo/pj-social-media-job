dbd080e88a4a1d756893597659c5845dc8a9ace7
cvukxwhoo <chienvuk@gmail.com>
2023-12-23 01:09:52 +0700

    header doing

--- client/src/components/Layout/HeaderOnly/Header/index.jsx
+++ client/src/components/Layout/HeaderOnly/Header/index.jsx
@@ -1,5 +1,33 @@
+import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <nav className='navbar fixed-top shrink'>
+        <div className='header-container'>
+          {/* NAV BRAND */}
+          <div className='nav-brand'>
+            <a href='#'>
+              <img
+                src='https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png'
+                alt=''
+              />
+            </a>
+          </div>
+
+          {/* SEARCH BAR */}
+          <label className='nav-search'>
+            <input type='search' />
+            <span>
+              <FontAwesomeIcon icon={faSearch} className='fa-light' />
+            </span>
+          </label>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
