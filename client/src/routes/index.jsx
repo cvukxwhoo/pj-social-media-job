19e0310b3e5a1500c9f825c494755a3fda0addb4
cvukxwhoo <chienvuk@gmail.com>
2023-12-23 12:26:31 +0700

    update header at Default Layout and header in Header Only

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,10 +1,12 @@
-import Home from '../pages/Home/index.jsx';
-import Login from '../pages/Login/index.jsx';
-import HeaderOnly from '../components/Layout/HeaderOnly/index.jsx';
+import Home from "../pages/Home/index.jsx";
+import Login from "../pages/Login/index.jsx";
+import HeaderOnly from "../components/Layout/HeaderOnly/index.jsx";
+import Employers from "../pages/Employers/index.jsx";
 
 const publicRoutes = [
-  { path: '/', component: Home, layout: HeaderOnly },
-  { path: '/login', component: Login },
+  { path: "/", component: Home, layout: HeaderOnly },
+  { path: "/login", component: Login },
+  { path: "/employers", component: Employers, layout: HeaderOnly },
 ];
 
 export { publicRoutes };
