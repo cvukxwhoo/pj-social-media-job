484e64c8bcf8934dbf2549cba24a66a32971b226
cvukxwhoo <chienvuk@gmail.com>
2023-12-24 11:50:23 +0700

    updating employers page

---
+++ client/src/pages/Employers/components/HighValueServices/index.jsx
@@ -0,0 +1,27 @@
+import "./styles.scss";
+
+const HighValueServices = () => {
+  return (
+    <>
+      <section className="high-value-service">
+        <div className="container">
+          <h1 className="title">High-value services for IT Employers </h1>
+          <div className="flex-box-wrapper">
+            <div className="flex-row-reverse">
+              <div className="justify-content-center">
+                <img src="" alt="" />
+              </div>
+              <div className="flex-1">
+                <h1></h1>
+                <p></p>
+                <div className="d-lg-flex"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HighValueServices;
--- client/src/pages/Employers/index.jsx
+++ client/src/pages/Employers/index.jsx
@@ -1,3 +1,5 @@
+import HireTheBestIT from "./components/HireTHeBestIT/index.jsx";
+import ITViecDifferent from "./components/ITViecDifferent/index.jsx";
 import "./styles.scss";
 
 const Employers = () => {
@@ -5,39 +7,8 @@ const Employers = () => {
     <main>
       <div className="flex-grow-1">
         <section className="employer-landing-container">
-          <section className="hire-the-best-it">
-            <div className="container">
-              {/* LEFT SIDE */}
-              <div className="left-side">
-                {/* Content */}
-                <div className="left-box-content">
-                  <h1 className="title">
-                    Hire the best IT Professionals in Vietnam with ITviec
-                  </h1>
-                  <p className="detailContent">
-                    With in-depth understanding in the IT sector and specialized
-                    skills, we can help you reach and hire the best IT
-                    candidates.
-                  </p>
-                  <button>Contact Now</button>
-                  <div className="accountAlreadyNav">
-                    <p> Already have an Employer account?</p>
-                    <p>
-                      <a href="#">Sign In</a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              {/* RIGHT SIDE */}
-              <div className="right-side">
-                <img
-                  src="https://itviec.com/assets/employer_landing/hire-the-best-it-15-95d4b6df6293a405cd77c094b8c7eb5dcc99cf8711f5b47751c841cfa51023a0.png"
-                  alt=""
-                />
-              </div>
-            </div>
-          </section>
+          <HireTheBestIT />
+          <ITViecDifferent />
         </section>
       </div>
     </main>
