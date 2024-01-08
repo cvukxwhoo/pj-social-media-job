8638880566d14932222125e7485921968f6090b0
SandyTa1 <taquang2008@gmail.com>
2024-01-08 13:04:34 +0700

    login + register

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,77 +1,118 @@
 import React, { useState } from "react";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { EyeInvisibleOutlined, EyeTwoTone, LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
-import "./styles.css";
-import { Link, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { useAuth } from "../../context/AuthContext";
+import "./styles.css";
 
 const Login = () => {
   const { login } = useAuth();
-  const [token, setToken] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   const onFinish = async (values) => {
     try {
       const loginpost = await axios.post("http://localhost:3001/login", values);
-      setToken(loginpost.data.token);
-      login(loginpost.data.data, loginpost.data.token);
+      const { token, data } = loginpost.data;
+      login(data, token);
     } catch (error) {
       console.log(error.response.data.message);
     }
   };
-  return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Email!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Email"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your Password!",
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <Link to="/register">register now!</Link>
-      </Form.Item>
-    </Form>
+  const handleShowPasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const [buttonColor, setButtonColor] = useState('#f00');
+
+  const handleButtonHover = () => {
+    setButtonColor('#800');
+  };
+
+  const handleButtonLeave = () => {
+    setButtonColor('#f00');
+  };
+
+  return (
+    <div className="parent-container">
+      <div className="first-container">
+        <div className="logo">
+          <h1>
+            Welcome to <span className="logo-text"><img className="logo-size" src="https://itviec.com/assets/logo_black_text-04776232a37ae9091cddb3df1973277252b12ad19a16715f4486e603ade3b6a4.png" alt="Logo" /></span>
+          </h1>
+        </div>
+        <div className="divider">
+          <hr />
+          <span>or</span>
+          <hr />
+        </div>
+        <Form className="email-form" onFinish={onFinish}>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Email!",
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Email"
+              autoComplete="off"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="Password"
+              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              visibilityToggle={false}
+              autoComplete="off"
+            />
+          </Form.Item>
+          <div className="forgot-password">
+            <Link to="#">Forgot Password?</Link>
+          </div>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="email-btn"
+            style={{
+              backgroundColor: buttonColor,
+              borderColor: buttonColor,
+              transition: 'background-color 0.3s, border-color 0.3s',
+            }}
+            onMouseEnter={handleButtonHover}
+            onMouseLeave={handleButtonLeave}
+          >
+            Sign in with Email
+          </Button>
+          <div className="signup-link">
+            Do not have an account? <Link to="/register">Sign up now!</Link>
+          </div>
+        </Form>
+      </div>
+      <div className="second-container">
+        <h2>Sign in to get instant access to thousands of reviews and salary information</h2>
+        <ul className="bullet-points">
+          <li>View salary to help you negotiate your offer or pay rise</li>
+          <li>Find out about benefits, interview, company culture via reviews</li>
+          <li>Easy apply with only 1 click</li>
+          <li>Manage your own profile & privacy</li>
+        </ul>
+      </div>
+    </div>
   );
 };
+
 export default Login;
--- client/src/pages/Register/index.jsx
+++ client/src/pages/Register/index.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import { Form, Input, Button, Radio } from "antd";
 import axios from "axios";
@@ -7,6 +8,7 @@ import Swal from "sweetalert2";
 
 const Register = () => {
   const navigate = useNavigate();
+
   const onFinish = async (values) => {
     try {
       const registerpost = await axios.post(
@@ -18,6 +20,7 @@ const Register = () => {
       register_info(error.response.data);
     }
   };
+
   const register_info = (registerpost) => {
     if (registerpost.message === "User created successfully") {
       Swal.fire({
@@ -35,11 +38,24 @@ const Register = () => {
       });
     }
   };
+
+  const [buttonColor, setButtonColor] = useState('#f00');
+
+  const handleButtonHover = () => {
+    setButtonColor('#800');
+  };
+
+  const handleButtonLeave = () => {
+    setButtonColor('#f00');
+  };
+
+
   const [selectedValue, setSelectedValue] = useState(null);
 
   const handleRadioChange = (e) => {
     setSelectedValue(e.target.value);
   };
+
   const validatePasswordLength = (rule, value, callback) => {
     if (value && value.length < 8) {
       callback("Password must be at least 8 characters long!");
@@ -47,93 +63,115 @@ const Register = () => {
       callback();
     }
   };
+
   return (
-    <Form name="registration_form" onFinish={onFinish} scrollToFirstError>
-      <Form.Item
-        name="userName"
-        label="Username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <div className="register-container">
+      <div className="logo-container">
+        <h1>
+          Welcome to <span className="logo-text"><img className="logo-size" src="https://itviec.com/assets/logo_black_text-04776232a37ae9091cddb3df1973277252b12ad19a16715f4486e603ade3b6a4.png" alt="Logo" /></span>
+        </h1>
+      </div>
+      <div className="form-container">
+        <Form name="registration_form" onFinish={onFinish} scrollToFirstError>
+          <Form.Item
+            name="userName"
+            label="Username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        name="email"
-        label="E-mail"
-        rules={[
-          {
-            type: "email",
-            message: "The input is not a valid e-mail address!",
-          },
-          {
-            required: true,
-            message: "Please input your e-mail!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not a valid e-mail address!",
+              },
+              {
+                required: true,
+                message: "Please input your e-mail!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-          { validator: validatePasswordLength },
-        ]}
-        hasFeedback
-      >
-        <Input.Password />
-      </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+              { validator: validatePasswordLength },
+            ]}
+            hasFeedback
+          >
+            <Input.Password />
+          </Form.Item>
 
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={["password"]}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please confirm your password!",
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(
-                new Error("The two passwords do not match!")
-              );
-            },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item name="role" label="Accout">
-        <Radio.Group onChange={handleRadioChange} value={selectedValue}>
-          <Radio value="individual">individual</Radio>
-          <Radio value="business">business</Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item>
-        <div className="register-button">
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-          Or
-          <Link to="/login"> return login!</Link>
-        </div>
-      </Form.Item>
-    </Form>
+          <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={["password"]}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password!",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error("The two passwords do not match!")
+                  );
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item name="role" label="Account">
+            <Radio.Group onChange={handleRadioChange} value={selectedValue}>
+              <Radio value="individual">Individual</Radio>
+              <Radio value="business">Business</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item>
+            <div className="register-button">
+              <Button 
+                className="red-button"
+                type="primary"
+                htmlType="submit" 
+                style={{
+                  backgroundColor: buttonColor,
+                  borderColor: buttonColor,
+                  transition: 'background-color 0.3s, border-color 0.3s',
+                }}
+                onMouseEnter={handleButtonHover}
+                onMouseLeave={handleButtonLeave}
+              >
+                Register
+              </Button>
+              Already have an account? <Link to="/login">Sign In Now!</Link>
+            </div>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 
