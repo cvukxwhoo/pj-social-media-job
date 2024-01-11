317376249fbe5a8068de644a1d08c7525f2a6f76
trananh-it-hust <haianhtranmanh@gmail.com>
2024-01-11 11:05:28 +0700

    a

--- server/controllers/infoUserController.js
+++ server/controllers/infoUserController.js
@@ -1,5 +1,11 @@
+import { ObjectId } from "mongodb";
+import EduModel from "../Models/Detail/Edu.js";
 import DetailUserModel from "../Models/DetailUser.js";
 import OptionModel from "../Models/Option.js";
+import ExpModel from "../Models/Detail/Exp.js";
+import SkillsModel from "../Models/Detail/Skills.js";
+import ProjectModel from "../Models/Detail/Project.js";
+import CoursesModel from "../Models/Detail/Courses.js";
 
 const infoUserController = {
   //Update dữ liệu
@@ -84,6 +90,104 @@ const infoUserController = {
         res.status(404).json({
           message: "Not found !",
           data: NewUpdateOption,
+        });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  },
+  getInfoDetailUser: async (req, res) => {
+    try {
+      req.id = { idUser: req.body.id };
+      const { name } = req.query;
+      let NewGetInfo;
+      switch (name) {
+        case "Education":
+          NewGetInfo = await EduModel.findOne(req.id);
+          break;
+        case "Exp":
+          NewGetInfo = await ExpModel.findOne(req.id);
+          break;
+        case "Skills":
+          NewGetInfo = await SkillsModel.findOne(req.id);
+          break;
+        case "Project":
+          NewGetInfo = await ProjectModel.findOne(req.id);
+          break;
+        case "Courses":
+          NewGetInfo = await CoursesModel.findOne(req.id);
+          break;
+        default:
+          res.status(500).json({
+            message: "Router is fail",
+          });
+          return;
+      }
+      if (NewGetInfo)
+        res.status(200).json({
+          message: "Get Succes !",
+          data: NewGetInfo,
+        });
+      else
+        res.status(404).json({
+          message: "Not found !",
+          data: NewGetInfo,
+        });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  },
+  putInfoDetailUser: async (req, res) => {
+    try {
+      const update = { $set: req.body };
+      req.id = { idUser: req.body.id };
+      const { name } = req.query;
+      let NewUpdateInfo;
+      switch (name) {
+        case "Education":
+          NewUpdateInfo = await EduModel.findOneAndUpdate(req.id, update, {
+            new: true,
+          });
+          break;
+        case "Exp":
+          NewUpdateInfo = await ExpModel.findOneAndUpdate(req.id, update, {
+            new: true,
+          });
+          break;
+        case "Skills":
+          NewUpdateInfo = await SkillsModel.findOneAndUpdate(req.id, update, {
+            new: true,
+          });
+          break;
+        case "Project":
+          NewUpdateInfo = await ProjectModel.findOneAndUpdate(req.id, update, {
+            new: true,
+          });
+          break;
+        case "Courses":
+          NewUpdateInfo = await CoursesModel.findOneAndUpdate(req.id, update, {
+            new: true,
+          });
+          break;
+        default:
+          res.status(500).json({
+            message: "Router is fail",
+          });
+          return;
+      }
+
+      if (NewUpdateInfo)
+        res.status(201).json({
+          message: "Update Succes !",
+          data: NewUpdateInfo,
+        });
+      else
+        res.status(404).json({
+          message: "Not found !",
+          data: NewUpdateInfo,
         });
     } catch (error) {
       res.status(500).json({
--- server/routes/infoUser.js
+++ server/routes/infoUser.js
@@ -15,5 +15,7 @@ infoUserRouter.put("/", middlewares.infoUser, infoUserController.putInfoUser);
 infoUserRouter.post("/", middlewares.infoUser, infoUserController.getInfoUser);
 infoUserRouter.post("/option", infoUserController.getOptionUser);
 infoUserRouter.put("/option", infoUserController.putOptionUser);
+infoUserRouter.post("/create/", infoUserController.getInfoDetailUser);
+infoUserRouter.put("/create/", infoUserController.putInfoDetailUser);
 
 export default infoUserRouter;
