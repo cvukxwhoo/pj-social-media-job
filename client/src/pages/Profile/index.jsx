19f5f293c9225b7ec9073ef560fd5b39caa1b48e
trananh-it-hust <haianhtranmanh@gmail.com>
2024-01-11 11:38:16 +0700

    fix

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -25,33 +25,31 @@ const Profile = () => {
 
   return (
     <>
-      {details.map((d) => (
-        <div className="profile-container" key={d._id}>
-          <div className="profile-header">
-            <div className="profile-header-content">
-              <div className="profile-header-row">
-                <div className="profile-avatar">
-                  <img
-                    className="profile-logo"
-                    src="https://itviec.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK1J1REE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--53ab4877387c44be52c1fda170364fa542dc4173/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oSnlaWE5wZW1WZmRHOWZabWwwV3dkcEFhb3ciLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--bb0ebae071595ab1791dc0ad640ef70a76504047/vietinbank-logo.png"
-                    alt=""
-                  />
+      <div className="profile-container" key={details._id}>
+        <div className="profile-header">
+          <div className="profile-header-content">
+            <div className="profile-header-row">
+              <div className="profile-avatar">
+                <img
+                  className="profile-logo"
+                  src="https://itviec.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBK1J1REE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--53ab4877387c44be52c1fda170364fa542dc4173/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBPZ2wzWldKd09oSnlaWE5wZW1WZmRHOWZabWwwV3dkcEFhb3ciLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--bb0ebae071595ab1791dc0ad640ef70a76504047/vietinbank-logo.png"
+                  alt=""
+                />
+              </div>
+              <div className="profile-header-ds">
+                <h1 className="profile-title">{details.userName} </h1>
+                <div className="profile-location">
+                  {" "}
+                  <FontAwesomeIcon icon={faLocationDot} />
+                  {details.location}
                 </div>
-                <div className="profile-header-ds">
-                  <h1 className="profile-title">{d.userName} </h1>
-                  <div className="profile-location">
-                    {" "}
-                    <FontAwesomeIcon icon={faLocationDot} />
-                    {d.location}
-                  </div>
-                  <button className="profile-header-follow">Follow</button>
-                </div>
+                <button className="profile-header-follow">Follow</button>
               </div>
             </div>
           </div>
-          <div className="test">{d.workPlace}</div>
         </div>
-      ))}
+        <div className="test">{details.workPlace}</div>
+      </div>
     </>
   );
 };
